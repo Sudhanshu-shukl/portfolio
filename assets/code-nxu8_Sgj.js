ae8b655c9d852a97f8cf0a0e996b6f66691951d1
Sudhanshu-shukl <shuklasudhanshu230405@gmail.com>
2025-07-17 01:13:29 +0530

    Updates

--- assets/code-nxu8_Sgj.js
+++ assets/code-nxu8_Sgj.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BKnB5ww5.js";/**
+import{c as e}from"./index-BXVxwC-O.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
